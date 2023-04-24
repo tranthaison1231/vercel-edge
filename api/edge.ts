bec1a3cc04c82a2afe403497720786e539b03541
son.tran <son.tran@team.enouvo.com>
2023-04-25 06:55:13 +0700

    feat(master): add error

--- api/edge.ts
+++ api/edge.ts
@@ -3,13 +3,24 @@ export const config = {
 };
 
 export default async function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?${searchParams.toString()}`);
-  const data = await response.json();
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: {
-      'content-type': 'application/json',
-    },
-  });
+  try {
+    const response = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=1h`
+    );
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        'Cache-Control': 'max-age=60',
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
